1e3672fa04dbc24c70214966340533074cc6b158
veggieemmy <emmyangelamoss@gmail.com>
2021-02-14 07:40:25 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+function formatDate(date) {
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[now.getDay()];
+    let hours = now.getHours();
+        if (hours < 10) {
+            hours = `0${hours}`;
+        }
+    let minutes = now.getMinutes();
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+    return `${day} ${hours}:${minutes}`
+}
+
+function displayWeatherCondition(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#description").innerHTML = response.data.weather[0].main;
+}
+function searchCity(city) {
+    let apiKey = "bd61a16d03e69c4265d6aac8396e35c8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#search-text-input").value;
+    searchCity(city);
+}
+
+function searchLocation(position) {
+    let apiKey = "bd61a16d03e69c4265d6aac8396e35c8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let now = new Date();
+let currently = document.querySelector("#current-date");
+currently.innerHTML = formatDate(currently);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Chicago");
+
+
